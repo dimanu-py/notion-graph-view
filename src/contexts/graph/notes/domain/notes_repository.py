ce8faae95b74fe932d2500cafa2adbe86051a11d
dimanu-py <diegomtz126@gmail.com>
2024-12-12 23:05:29 +0100

    refactor(notes): make NotesRepository an interface

--- src/contexts/graph/notes/domain/notes_repository.py
+++ src/contexts/graph/notes/domain/notes_repository.py
@@ -1,3 +1,7 @@
-class NotesRepository:
+from abc import ABC, abstractmethod
+
+
+class NotesRepository(ABC):
+    @abstractmethod
     def search(self, database_id: str) -> dict:
         raise NotImplementedError
