194304341014d3f126dd2ee8ee365c510112d172
dimanu-py <diegomtz126@gmail.com>
2024-12-12 22:56:39 +0100

    feat(test-notes): create basic unit test configuration for notes module and use it inside all notes finder test

--- tests/contexts/graph/notes/application/test_all_notes_finder.py
+++ tests/contexts/graph/notes/application/test_all_notes_finder.py
@@ -1,19 +1,21 @@
 import pytest
-from doublex import Spy, assert_that, called
 
 from src.contexts.graph.notes.application.all_notes_finder import AllNotesFinder
-from src.contexts.graph.notes.domain.notes_repository import NotesRepository
+from tests.contexts.graph.notes.notes_module_unit_test_config import (
+    NotesModuleUnitTestConfig,
+)
 
 
-@pytest.mark.unit
-class TestNotesFinder:
+class TestAllNotesFinder(NotesModuleUnitTestConfig):
     ANY_ID = "any_id"
 
+    def setup_method(self) -> None:
+        self.finder = AllNotesFinder(repository=self.repository)
+
     @pytest.mark.xfail
-    def test_finds_notion_notes(self) -> None:
-        repository = Spy(NotesRepository)
-        notes_finder = AllNotesFinder(repository)
+    def test_should_find_all_notes_in_a_database(self) -> None:
+        self.should_search(database_id=self.ANY_ID)
 
-        notes_finder(database_id=self.ANY_ID)
+        self.finder(database_id=self.ANY_ID)
 
-        assert_that(repository.search, called().with_args(self.ANY_ID))
+        self.should_have_searched()
