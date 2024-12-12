76e93dfd6127bbf6f2bb14fb796ee2a2ba83de55
dimanu-py <diegomtz126@gmail.com>
2024-12-12 23:06:47 +0100

    refactor(delivery-notes): use concrete NotesRepository implementation in controller

--- src/delivery/api/main.py
+++ src/delivery/api/main.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI
 
 from src.contexts.graph.notes.application.all_notes_finder import AllNotesFinder
-from src.contexts.graph.notes.domain.notes_repository import NotesRepository
-
+from src.contexts.graph.notes.infra.notion_notes_repository import NotionNotesRepository
 
 app = FastAPI()
 
 
 @app.get("/graph/{database_id}")
 async def get_database_notes(database_id: str) -> dict:
-    notes_finder = AllNotesFinder(repository=NotesRepository())
+    notes_finder = AllNotesFinder(repository=NotionNotesRepository())
     notes = notes_finder(database_id)
     return notes
