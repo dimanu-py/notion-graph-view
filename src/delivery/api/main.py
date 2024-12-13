28c08902aa8393461d6badf3dbb42165d4833079
dimanu-py <diegomtz126@gmail.com>
2024-12-13 13:48:21 +0100

    refactor(delivery-notes): inject NotionClient into repository using env variables

--- src/delivery/api/main.py
+++ src/delivery/api/main.py
@@ -1,13 +1,23 @@
-from fastapi import FastAPI
+import os
 
 from src.contexts.graph.notes.application.all_notes_finder import AllNotesFinder
+from src.contexts.graph.notes.infra.notion_client import NotionClient
 from src.contexts.graph.notes.infra.notion_notes_repository import NotionNotesRepository
 
 app = FastAPI()
 
+load_dotenv()
+
+
+def inject_client() -> NotionClient:
+    return NotionClient(auth_token=os.environ["NOTION_API_KEY"])
+
 
 @app.get("/graph/{database_id}")
-async def get_database_notes(database_id: str) -> dict:
-    notes_finder = AllNotesFinder(repository=NotionNotesRepository())
+async def get_database_notes(
+    database_id: str, client: NotionClient = Depends(inject_client)
+) -> JSONResponse:
+    notes_finder = AllNotesFinder(repository=NotionNotesRepository(client=client))
+
     notes = notes_finder(database_id)
     return notes
