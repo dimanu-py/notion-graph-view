904fd21bc1443c4105ef6c396670511c862daadb
dimanu-py <diegomtz126@gmail.com>
2024-12-13 12:07:18 +0100

    refactor(notes): stablish that use case and repository return a list of notes

--- src/contexts/graph/notes/application/all_notes_finder.py
+++ src/contexts/graph/notes/application/all_notes_finder.py
@@ -1,3 +1,4 @@
+from src.contexts.graph.notes.domain.note import Note
 from src.contexts.graph.notes.domain.notes_database_id import NotesDatabaseId
 from src.contexts.graph.notes.domain.notes_repository import NotesRepository
 
@@ -8,5 +9,5 @@ class AllNotesFinder:
     def __init__(self, repository: NotesRepository) -> None:
         self._repository = repository
 
-    def __call__(self, database_id: str) -> dict:
+    def __call__(self, database_id: str) -> list[Note]:
         return self._repository.search(database_id=NotesDatabaseId(database_id))
--- src/contexts/graph/notes/domain/notes_repository.py
+++ src/contexts/graph/notes/domain/notes_repository.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
 
+from src.contexts.graph.notes.domain.note import Note
 from src.contexts.graph.notes.domain.notes_database_id import NotesDatabaseId
 
 
 class NotesRepository(ABC):
     @abstractmethod
-    def search(self, database_id: NotesDatabaseId) -> dict:
+    def search(self, database_id: NotesDatabaseId) -> list[Note]:
         raise NotImplementedError
