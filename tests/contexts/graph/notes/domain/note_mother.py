ccf5b1f27b57972a7d089e3e895ac529ae3eac17
dimanu-py <diegomtz126@gmail.com>
2024-12-13 17:42:51 +0100

    refactor(test-notes): modify how NoteMother works not using the file
    Now the object mother creates the object defining how I want the data to be passed

--- tests/contexts/graph/notes/domain/note_mother.py
+++ tests/contexts/graph/notes/domain/note_mother.py
@@ -1,16 +1,24 @@
-import json
-
 from src.contexts.graph.notes.domain.note import Note
+from tests.contexts.graph.notes.domain.note_id_mother import NoteIdMother
+from tests.contexts.graph.notes.domain.note_title_mother import NoteTitleMother
+from tests.contexts.graph.notes.domain.note_url_mother import NoteUrlMother
+from tests.contexts.graph.notes.domain.related_notes_mother import RelatedNotesMother
 
 
 class NoteMother:
     @classmethod
-    def create(cls, number: int) -> Note:
-        with open("notes.json") as file:
-            note = json.load(file)["results"][number]
+    def create(cls, params: dict) -> Note:
+        primitives = {
+            "id": NoteIdMother.create().value,
+            "url": NoteUrlMother.create().value,
+            "title": NoteTitleMother.create().value,
+            "related_notes": RelatedNotesMother.create().value,
+            **(params if params else {}),
+        }
+
         return Note.create(
-            id_=note["id"],
-            url=note["url"],
-            title=note["properties"]["Name"]["title"][0]["plain_text"],
-            related_notes=note["properties"]["Related Notes"],
+            id_=primitives["id"],
+            url=primitives["url"],
+            title=primitives["title"],
+            related_notes=primitives["related_notes"],
         )
--- tests/contexts/graph/notes/infra/test_notion_notes_repository.py
+++ tests/contexts/graph/notes/infra/test_notion_notes_repository.py
@@ -21,7 +21,23 @@ class TestNotionNotesRepository:
         self.repository = NotionNotesRepository(client=self.notion_client)
 
     def test_should_search_all_notes(self) -> None:
-        expected_notes = [NoteMother.create(0), NoteMother.create(1)]
+        note_one = NoteMother.create(
+            {
+                "id": "15bf5bab-5d4e-807b-bf58-ca937660b2fb",
+                "url": "https://www.notion.so/Note-test-integration-2-15bf5bab5d4e807bbf58ca937660b2fb",
+                "title": "Note test integration 2",
+                "related_notes": ["15bf5bab-5d4e-8064-b9ce-dd66d2024fc6"],
+            }
+        )
+        note_two = NoteMother.create(
+            {
+                "id": "15bf5bab-5d4e-8064-b9ce-dd66d2024fc6",
+                "url": "https://www.notion.so/Note-test-integration-1-15bf5bab5d4e8064b9cedd66d2024fc6",
+                "title": "Note test integration 1",
+                "related_notes": ["15bf5bab-5d4e-807b-bf58-ca937660b2fb"],
+            }
+        )
+        expected_notes = [note_one, note_two]
         database_id = NotesDatabaseIdMother.create(
             os.environ["NOTION_TEST_DATABASE_ID"]
         )
