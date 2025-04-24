d24b270bbdf6bb5f434c160307d593bbe3801b59
dimanu-py <diegomtz126@gmail.com>
2025-04-24 19:39:29 +0200

    test(notes): add unit test to verify integration with notion api and fetching notes of a database

---
+++ backend/contexts/notes/src/main/kotlin/com/dimanupy/notes/note/infra/HttpNotionRepository.kt
@@ -0,0 +1,10 @@
+package com.dimanupy.notes.note.infra
+
+import com.dimanupy.notes.note.domain.Note
+import com.dimanupy.notes.note.domain.NotionRepository
+
+class HttpNotionRepository : NotionRepository {
+    override fun sync(databaseId: String): List<Note> {
+        throw NotImplementedError()
+    }
+}
---
+++ backend/contexts/notes/src/test/kotlin/com/dimanupy/notes/note/infra/HttpNotionRepositoryShould.kt
@@ -0,0 +1,35 @@
+package com.dimanupy.notes.note.infra
+
+import com.dimanupy.notes.note.domain.NoteMother
+import org.junit.jupiter.api.BeforeEach
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class HttpNotionRepositoryShould {
+
+    private lateinit var httpNotionRepository: HttpNotionRepository
+
+    @BeforeEach
+    fun setUp() {
+        httpNotionRepository = HttpNotionRepository()
+    }
+
+    @Test
+    fun `fetch notes from valid Notion database`() {
+        val databaseId = System.getenv("TEST_DATABASE_ID")
+        val expectedNotes = listOf(
+            NoteMother.create(
+                url = "https://www.notion.so/Note-test-integration-2-15bf5bab5d4e807bbf58ca937660b2fb",
+                title = "Note test integration 2",
+            ),
+            NoteMother.create(
+                url = "https://www.notion.so/Note-test-integration-1-15bf5bab5d4e8064b9cedd66d2024fc6",
+                title = "Note test integration 1",
+            )
+        )
+
+        val notes = httpNotionRepository.sync(databaseId)
+
+        assertEquals(expectedNotes, notes)
+    }
+}
