62c7cabc02c32b6cafaaa83673acc66b2a4dbbed
dimanu-py <diegomtz126@gmail.com>
2025-04-24 11:20:50 +0200

    test(notes): create any method in NoteMother to create random Notes semantically

--- backend/contexts/notes/src/test/kotlin/com/dimanupy/notes/note/domain/NoteMother.kt
+++ backend/contexts/notes/src/test/kotlin/com/dimanupy/notes/note/domain/NoteMother.kt
@@ -2,6 +2,10 @@ package com.dimanupy.notes.note.domain
 
 object NoteMother {
 
+    fun any(): Note {
+        return Note(title = NoteTitleMother.create(), url = NoteUrlMother.create())
+    }
+
     fun create(title: String, url: String): Note {
         return Note(title = NoteTitleMother.create(title), url = NoteUrlMother.create(url))
     }
