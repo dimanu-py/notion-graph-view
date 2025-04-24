d66f348ed3dbcc79792ca7398fb8254c6ce4b79f
dimanu-py <diegomtz126@gmail.com>
2025-04-24 18:45:52 +0200

    build: add http4k and json dependencies to contexts subproject
    Newest version is not used due to incompatible build version with project kotlin jvm

--- backend/contexts/notes/build.gradle.kts
+++ backend/contexts/notes/build.gradle.kts
@@ -11,6 +11,9 @@ repositories {
 }
 
 dependencies {
+    implementation ("org.http4k:http4k-core:5.12.0.0")
+    implementation("org.json:json:20250107")
+
     testImplementation("com.github.javafaker:javafaker:1.0.2") {
         exclude(group = "org.yaml", module = "snakeyaml")
     }
