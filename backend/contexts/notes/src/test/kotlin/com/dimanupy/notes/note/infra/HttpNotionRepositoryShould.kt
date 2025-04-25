163c93f3c12f6ee46f8fe005981255ced8b2e747
dimanu-py <diegomtz126@gmail.com>
2025-04-25 18:39:21 +0200

    test(notes): add integration test to verify exception is thrown when the database is not shared with notion's integration

--- backend/contexts/notes/src/test/kotlin/com/dimanupy/notes/note/infra/HttpNotionRepositoryShould.kt
+++ backend/contexts/notes/src/test/kotlin/com/dimanupy/notes/note/infra/HttpNotionRepositoryShould.kt
@@ -3,6 +3,7 @@ package com.dimanupy.notes.note.infra
 import com.dimanupy.notes.note.domain.NoteMother
 import org.http4k.client.JavaHttpClient
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.assertThrows
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -33,5 +34,16 @@ class HttpNotionRepositoryShould {
         val notes = httpNotionRepository.fetch(databaseId)
 
         assertEquals(expectedNotes, notes)
+    }
+
+    @Test
+    fun `throw error if database has not integration set`() {
+        val databaseId = "e7141ea7-a862-4f70-bb6a-b7ce0fe0eb3f"
+
+        val error = assertThrows<InvalidNotionDatabase> {
+            httpNotionRepository.fetch(databaseId)
+        }
+
+        assertEquals("Database with id $databaseId is not shared with your integration.", error.message)
     }
 }
