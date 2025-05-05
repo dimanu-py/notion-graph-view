185f14e511290330d6abe56326e5294f32e83b58
dimanu-py <diegomtz126@gmail.com>
2025-05-05 19:30:18 +0200

    feat(notes): implement save method in PostgresNotesRepository to persist notes

--- backend/contexts/notes/src/main/kotlin/com/dimanupy/notes/note/infra/PostgresNotesRepository.kt
+++ backend/contexts/notes/src/main/kotlin/com/dimanupy/notes/note/infra/PostgresNotesRepository.kt
@@ -2,9 +2,18 @@ package com.dimanupy.notes.note.infra
 
 import com.dimanupy.notes.note.domain.Note
 import com.dimanupy.notes.note.domain.NotesRepository
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.transactions.transaction
 
 class PostgresNotesRepository: NotesRepository {
     override fun save(notes: List<Note>) {
-        throw NotImplementedError()
+        transaction {
+            notes.forEach { note ->
+                NotesModel.insert {
+                    it[title] = note.title.value
+                    it[url] = note.url.value
+                }
+            }
+        }
     }
 }
