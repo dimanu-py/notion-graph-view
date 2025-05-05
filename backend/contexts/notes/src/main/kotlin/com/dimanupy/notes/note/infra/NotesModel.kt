b9b93324b95a6197f074f0defbad9103c228e901
dimanu-py <diegomtz126@gmail.com>
2025-05-05 19:52:21 +0200

    feat(notes): add fromPrimitives method to populate InsertStatement from a map

--- backend/contexts/notes/src/main/kotlin/com/dimanupy/notes/note/infra/NotesModel.kt
+++ backend/contexts/notes/src/main/kotlin/com/dimanupy/notes/note/infra/NotesModel.kt
@@ -1,8 +1,14 @@
 package com.dimanupy.notes.note.infra
 
 import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.statements.InsertStatement
 
 object NotesModel : Table("notes") {
     val title = varchar("title", 255)
     val url = varchar("url", 255)
+
+    fun fromPrimitives(data: Map<String, String>, statement: InsertStatement<Number>) {
+        statement[title] = data["title"]!!
+        statement[url] = data["url"]!!
+    }
 }
