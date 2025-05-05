eb8fa9f3191e6b09aec6e9798d7fc230847a2bf1
dimanu-py <diegomtz126@gmail.com>
2025-05-05 19:57:53 +0200

    refactor(notes): update save method to use batch insert for batch saving notes

--- backend/contexts/notes/src/main/kotlin/com/dimanupy/notes/note/infra/NotesModel.kt
+++ backend/contexts/notes/src/main/kotlin/com/dimanupy/notes/note/infra/NotesModel.kt
@@ -1,13 +1,13 @@
 package com.dimanupy.notes.note.infra
 
 import org.jetbrains.exposed.sql.Table
-import org.jetbrains.exposed.sql.statements.InsertStatement
+import org.jetbrains.exposed.sql.statements.BatchInsertStatement
 
 object NotesModel : Table("notes") {
     val title = varchar("title", 255)
     val url = varchar("url", 255)
 
-    fun fromPrimitives(data: Map<String, String>, statement: InsertStatement<Number>) {
+    fun fromPrimitives(data: Map<String, String>, statement: BatchInsertStatement) {
         statement[title] = data["title"]!!
         statement[url] = data["url"]!!
     }
--- backend/contexts/notes/src/main/kotlin/com/dimanupy/notes/note/infra/PostgresNotesRepository.kt
+++ backend/contexts/notes/src/main/kotlin/com/dimanupy/notes/note/infra/PostgresNotesRepository.kt
@@ -2,16 +2,14 @@ package com.dimanupy.notes.note.infra
 
 import com.dimanupy.notes.note.domain.Note
 import com.dimanupy.notes.note.domain.NotesRepository
-import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.batchInsert
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class PostgresNotesRepository: NotesRepository {
     override fun save(notes: List<Note>) {
         transaction {
-            notes.forEach { note ->
-                NotesModel.insert {
-                    fromPrimitives(note.toPrimitives(), it)
-                }
+            NotesModel.batchInsert(notes) { note ->
+                NotesModel.fromPrimitives(note.toPrimitives(), this)
             }
         }
     }
