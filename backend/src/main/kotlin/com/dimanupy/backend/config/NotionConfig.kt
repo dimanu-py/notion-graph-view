7c19f9fd1d75ba3223dc0b3b4f47170d3cf0b9f1
dimanu-py <diegomtz126@gmail.com>
2025-05-06 12:43:49 +0200

    feat(application): add database configuration to NotionConfig class

--- backend/src/main/kotlin/com/dimanupy/backend/config/NotionConfig.kt
+++ backend/src/main/kotlin/com/dimanupy/backend/config/NotionConfig.kt
@@ -2,7 +2,10 @@ package com.dimanupy.backend.config
 
 import com.dimanupy.notes.note.infra.HttpNotionRepository
 import com.dimanupy.notes.note.infra.NotionConnectionData
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import org.http4k.client.JavaHttpClient
+import org.jetbrains.exposed.sql.Database
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -18,4 +21,21 @@ class NotionConfig {
         client = JavaHttpClient(),
         connectionData = notionConnectionData
     )
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    fun databaseConnectionData() = DatabaseConnectionData()
+
+    @Bean
+    fun database(databaseConnectionData: DatabaseConnectionData): Database {
+        val hikariConfig = HikariConfig().apply {
+            jdbcUrl         = databaseConnectionData.url
+            username        = databaseConnectionData.username
+            password        = databaseConnectionData.password
+            driverClassName = "org.postgresql.Driver"
+            maximumPoolSize = 10
+        }
+        val dataSource = HikariDataSource(hikariConfig)
+        return Database.connect(dataSource)
+    }
 }
