7865c100268e642ba9e11e60229a2d3982400c65
dimanu-py <diegomtz126@gmail.com>
2025-05-06 08:08:44 +0200

    feat(notes): add toAggregate method to convert ResultRow to Note aggregate

--- backend/contexts/notes/src/main/kotlin/com/dimanupy/notes/note/infra/NotesModel.kt
+++ backend/contexts/notes/src/main/kotlin/com/dimanupy/notes/note/infra/NotesModel.kt
@@ -1,5 +1,7 @@
 package com.dimanupy.notes.note.infra
 
+import com.dimanupy.notes.note.domain.Note
+import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.statements.BatchInsertStatement
 
@@ -10,5 +12,12 @@ object NotesModel : Table("notes") {
     fun fromPrimitives(data: Map<String, String>, statement: BatchInsertStatement) {
         statement[title] = data["title"]!!
         statement[url] = data["url"]!!
+    }
+
+    fun toAggregate(note: ResultRow): Note {
+        return Note.fromPrimitives(
+            title = note[title],
+            url = note[url]
+        )
     }
 }
