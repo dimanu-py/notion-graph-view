4552c8a18cf5194adfd35ce8fe57fc70597186b8
dimanu-py <diegomtz126@gmail.com>
2025-05-08 15:50:58 +0200

    feat(notes): enhance syncNotes controller to handle Notion errors with appropriate responses

--- backend/src/main/kotlin/com/dimanupy/backend/notes/rest/SyncNotesController.kt
+++ backend/src/main/kotlin/com/dimanupy/backend/notes/rest/SyncNotesController.kt
@@ -1,6 +1,10 @@
 package com.dimanupy.backend.notes.rest
 
 import com.dimanupy.notes.note.application.NotesSyncer
+import com.dimanupy.notes.note.domain.InvalidDatabaseIdFormat
+import com.dimanupy.notes.note.domain.InvalidNotionDatabase
+import com.dimanupy.notes.note.domain.NotionError
+import com.dimanupy.notes.note.domain.UnexpectedNotionException
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PutMapping
@@ -13,7 +17,16 @@ class SyncNotesController(private val notesSyncer: NotesSyncer) {
 
     @PutMapping("/sync/{databaseId}")
     fun syncNotes(@PathVariable databaseId: String): ResponseEntity<String> {
-        notesSyncer(databaseId)
-        return ResponseEntity.ok().build()
+        return try {
+            notesSyncer(databaseId)
+            ResponseEntity.ok().build()
+        } catch (error: NotionError) {
+            when (error) {
+                is InvalidNotionDatabase,
+                is InvalidDatabaseIdFormat,
+                is UnexpectedNotionException -> ResponseEntity.badRequest().build()
+                else -> ResponseEntity.internalServerError().build()
+            }
+        }
     }
 }
