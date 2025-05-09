8ce23c1945135a77ea31b24918df9d55f579e881
dimanu-py <diegomtz126@gmail.com>
2025-05-09 23:25:13 +0200

    feat(notes): add NoteError handler to sync notes controller

--- backend/src/main/kotlin/com/dimanupy/backend/notes/rest/SyncNotesController.kt
+++ backend/src/main/kotlin/com/dimanupy/backend/notes/rest/SyncNotesController.kt
@@ -1,10 +1,7 @@
 package com.dimanupy.backend.notes.rest
 
 import com.dimanupy.notes.note.application.NotesSyncer
-import com.dimanupy.notes.note.domain.InvalidDatabaseIdFormat
-import com.dimanupy.notes.note.domain.InvalidNotionDatabase
-import com.dimanupy.notes.note.domain.NotionError
-import com.dimanupy.notes.note.domain.UnexpectedNotionException
+import com.dimanupy.notes.note.domain.*
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PutMapping
@@ -25,6 +22,13 @@ class SyncNotesController(private val notesSyncer: NotesSyncer) {
                 is InvalidNotionDatabase,
                 is InvalidDatabaseIdFormat,
                 is UnexpectedNotionException -> ResponseEntity.badRequest().build()
+                else -> ResponseEntity.internalServerError().build()
+            }
+        } catch (error: NoteError) {
+            when (error) {
+                is NoteUrlInvalidFormat,
+                is NoteUrlCannotBeEmpty,
+                is NoteTitleCannotBeEmpty -> ResponseEntity.badRequest().build()
                 else -> ResponseEntity.internalServerError().build()
             }
         }
