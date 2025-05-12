d4441da2d039d27a26319a0bc8b13a1532d68ef6
dimanu-py <diegomtz126@gmail.com>
2025-05-12 16:18:46 +0200

    refactor(notes): update create method in NoteMother to use default values for parameters

--- backend/contexts/notes/src/test/kotlin/com/dimanupy/notes/note/domain/NoteMother.kt
+++ backend/contexts/notes/src/test/kotlin/com/dimanupy/notes/note/domain/NoteMother.kt
@@ -4,7 +4,11 @@ object NoteMother {
 
     fun any(): Note = Note(notionNoteId = NotionNoteIdMother.create(), title = NoteTitleMother.create(), url = NoteUrlMother.create())
 
-    fun create(notionId: String, url: String, title: String): Note = Note(
+    fun create(
+        notionId: String = NotionNoteIdMother.create().value,
+        url: String = NoteUrlMother.create().value,
+        title: String = NoteTitleMother.create().value
+    ): Note = Note(
         notionNoteId = NotionNoteIdMother.create(notionId),
         title = NoteTitleMother.create(title),
         url = NoteUrlMother.create(url),
