f1f7ddb8562916591947f66f037c24b188506003
dimanu-py <diegomtz126@gmail.com>
2025-05-12 16:26:58 +0200

    refactor(notes): add length validation to NotionNoteId

--- backend/contexts/notes/src/main/kotlin/com/dimanupy/notes/note/domain/NotionNoteId.kt
+++ backend/contexts/notes/src/main/kotlin/com/dimanupy/notes/note/domain/NotionNoteId.kt
@@ -2,9 +2,21 @@ package com.dimanupy.notes.note.domain
 
 import com.dimanupy.notes.shared.domain.ValueObject
 
+
 class NotionNoteId(private val _value: String): ValueObject<String>(_value) {
+    companion object {
+        private const val REQUIRED_LENGTH = 32
+    }
+
     override fun validate(value: String) {
         ensureIsNotEmpty(value)
+        ensureLengthIsCorrect(value)
+    }
+
+    private fun ensureLengthIsCorrect(value: String) {
+        if (value.length != REQUIRED_LENGTH) {
+            throw InvalidNotionNoteIdFormat(value)
+        }
     }
 
     private fun ensureIsNotEmpty(value: String) {
