0b3f0eaef899146f891555379238be01f1dcdf25
dimanu-py <diegomtz126@gmail.com>
2025-05-13 21:32:39 +0200

    test(notes): pass relatedNotes value to NoteMother

--- backend/contexts/notes/src/test/kotlin/com/dimanupy/notes/note/domain/NoteMother.kt
+++ backend/contexts/notes/src/test/kotlin/com/dimanupy/notes/note/domain/NoteMother.kt
@@ -2,15 +2,22 @@ package com.dimanupy.notes.note.domain
 
 object NoteMother {
 
-    fun any(): Note = Note.fromPrimitives(notionId = NotionNoteIdPrimitivesMother.create(), title = NoteTitlePrimitivesMother.create(), url = NoteUrlPrimitivesMother.create())
+    fun any(): Note = Note.fromPrimitives(
+        notionId = NotionNoteIdPrimitivesMother.create(),
+        title = NoteTitlePrimitivesMother.create(),
+        url = NoteUrlPrimitivesMother.create(),
+        relatedNotes = NoteRelatedNotesPrimitivesMother.create()
+    )
 
     fun create(
         notionId: String = NotionNoteIdPrimitivesMother.create(),
         url: String = NoteUrlPrimitivesMother.create(),
         title: String = NoteTitlePrimitivesMother.create(),
+        relatedNotes: List<String> = NoteRelatedNotesPrimitivesMother.create(),
     ): Note = Note.fromPrimitives(
         notionId = notionId,
         title = title,
         url = url,
+        relatedNotes = relatedNotes,
     )
 }
