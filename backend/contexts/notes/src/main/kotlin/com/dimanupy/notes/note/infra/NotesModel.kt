df3922d79b77f02eee4e69a334d102e39d9bbe03
dimanu-py <diegomtz126@gmail.com>
2025-05-13 21:55:12 +0200

    feat(notes): add relatedNotes to database model

--- backend/contexts/notes/src/main/kotlin/com/dimanupy/notes/note/infra/NotesModel.kt
+++ backend/contexts/notes/src/main/kotlin/com/dimanupy/notes/note/infra/NotesModel.kt
@@ -1,8 +1,12 @@
 package com.dimanupy.notes.note.infra
 
 import com.dimanupy.notes.note.domain.Note
+import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.json.Json
 import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.json.jsonb
 import org.jetbrains.exposed.sql.statements.BatchInsertStatement
 
 object NotesModel : Table("notes") {
@@ -10,17 +14,25 @@ object NotesModel : Table("notes") {
     private val notionId = varchar("notion_id", 255)
     private val title = varchar("title", 255)
     private val url = varchar("url", 255)
+    private val relatedNotes = jsonb(
+        name = "related_notes",
+        jsonConfig = Json { encodeDefaults = true },
+        kSerializer = ListSerializer(String.serializer())
+    )
     override val primaryKey = PrimaryKey(id)
 
     fun fromPrimitives(data: Map<String, String>, statement: BatchInsertStatement) {
         statement[notionId] = data["notionId"]!!
         statement[title] = data["title"]!!
         statement[url] = data["url"]!!
+        statement[relatedNotes] = Json
+            .decodeFromString(ListSerializer(String.serializer()), data.getValue("relatedNotes"))
     }
 
     fun toAggregate(note: ResultRow): Note = Note.fromPrimitives(
         notionId = note[notionId],
         title = note[title],
         url = note[url],
+        relatedNotes = note[relatedNotes]
     )
 }
