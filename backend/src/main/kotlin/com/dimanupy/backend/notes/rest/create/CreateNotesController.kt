8bb779f6ac8d5636c6573f1e485b889a939b91e4
dimanu-py <diegomtz126@gmail.com>
2025-05-16 20:31:52 +0200

    feat(notes): update create notes controller to call note creator use case with note primitives

--- backend/src/main/kotlin/com/dimanupy/backend/notes/rest/create/CreateNotesController.kt
+++ backend/src/main/kotlin/com/dimanupy/backend/notes/rest/create/CreateNotesController.kt
@@ -1,6 +1,7 @@
 package com.dimanupy.backend.notes.rest.create
 
 import com.dimanupy.notes.note.application.create.NoteCreator
+import com.dimanupy.notes.note.domain.NotePrimitives
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -13,7 +14,12 @@ class CreateNotesController(private val noteCreator: NoteCreator) {
 
     @PutMapping
     fun createNote(@RequestBody request: CreateNoteRequest): ResponseEntity<String> {
-
+        noteCreator(NotePrimitives(
+            notionId = request.id,
+            title = request.title,
+            url = request.url,
+            relatedNotes = request.relatedNotes
+        ))
         return ResponseEntity.ok().build()
     }
 
