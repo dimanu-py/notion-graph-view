017db7de25ff117f4fea918dfe1a38ee5d7ea28e
dimanu-py <diegomtz126@gmail.com>
2025-05-16 17:29:14 +0200

    feat(notes): implement NoteCreator invoke method to save notes

--- backend/contexts/notes/src/main/kotlin/com/dimanupy/notes/note/application/create/NoteCreator.kt
+++ backend/contexts/notes/src/main/kotlin/com/dimanupy/notes/note/application/create/NoteCreator.kt
@@ -1,10 +1,12 @@
 package com.dimanupy.notes.note.application.create
 
+import com.dimanupy.notes.note.domain.Note
 import com.dimanupy.notes.note.domain.NotePrimitives
 import com.dimanupy.notes.note.domain.NotesRepository
 
 class NoteCreator(private val repository: NotesRepository) {
-    operator fun invoke(note: NotePrimitives) {
-        throw NotImplementedError()
+    operator fun invoke(noteInfo: NotePrimitives) {
+        val note = Note.fromPrimitives(noteInfo)
+        repository.save(note)
     }
 }
