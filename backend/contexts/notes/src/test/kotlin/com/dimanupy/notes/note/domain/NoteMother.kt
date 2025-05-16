28305c69901c9deef874dfd02e0b4119317348af
dimanu-py <diegomtz126@gmail.com>
2025-05-16 17:01:55 +0200

    refactor(notes): remove unused any method from NoteMother

--- backend/contexts/notes/src/test/kotlin/com/dimanupy/notes/note/domain/NoteMother.kt
+++ backend/contexts/notes/src/test/kotlin/com/dimanupy/notes/note/domain/NoteMother.kt
@@ -2,13 +2,6 @@ package com.dimanupy.notes.note.domain
 
 object NoteMother {
 
-    fun any(): NotePrimitives = NotePrimitives(
-        notionId = NotionNoteIdPrimitivesMother.create(),
-        title = NoteTitlePrimitivesMother.create(),
-        url = NoteUrlPrimitivesMother.create(),
-        relatedNotes = NoteRelatedNotesPrimitivesMother.create(),
-    )
-
     fun create(
         notionId: String = NotionNoteIdPrimitivesMother.create(),
         url: String = NoteUrlPrimitivesMother.create(),
