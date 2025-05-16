1586be18f95a43322bfca5db64a5dbc05a09ff8a
dimanu-py <diegomtz126@gmail.com>
2025-05-16 20:36:19 +0200

    test(notes): add test for handling empty url in note creation

--- backend/src/test/kotlin/com/dimanupy/backend/notes/rest/create/CreateNotesControllerShould.kt
+++ backend/src/test/kotlin/com/dimanupy/backend/notes/rest/create/CreateNotesControllerShould.kt
@@ -6,6 +6,7 @@ import io.restassured.module.kotlin.extensions.Given
 import io.restassured.module.kotlin.extensions.Then
 import io.restassured.module.kotlin.extensions.When
 import io.restassured.response.Response
+import org.hamcrest.CoreMatchers
 import org.junit.jupiter.api.BeforeEach
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.server.LocalServerPort
@@ -34,6 +35,20 @@ class CreateNotesControllerShould {
         thenStatusCodeIs(200)
     }
 
+    @Test
+    fun `throw error if note request has empty url`() {
+        givenANoteRequestWithEmptyUrl()
+
+        whenICreateANote()
+
+        thenStatusCodeIs(400)
+        thenResponseBodyContains(
+            "Bad Request",
+            "Note URL cannot be empty",
+            "/notes",
+        )
+    }
+
     private fun whenICreateANote() {
         response = Given {
             contentType(ContentType.JSON)
@@ -58,6 +73,26 @@ class CreateNotesControllerShould {
                 "related_notes": ["1f91481c-2f0c-4b23-8d50-e82ca05be8a2"]
             }
         """.trimIndent()
+    }
+
+    private fun givenANoteRequestWithEmptyUrl() {
+        requestBody = """
+            {
+                "id": "feb562ba-434a-45a0-93e0-4a233e31989c",
+                "title": "Test Note",
+                "url": " ",
+                "related_notes": ["1f91481c-2f0c-4b23-8d50-e82ca05be8a2"]
+            }
+        """.trimIndent()
+    }
+
+    private fun thenResponseBodyContains(error: String, message: String, path: String) {
+        response.Then {
+            body("occurredOn", CoreMatchers.notNullValue())
+            body("error", CoreMatchers.equalTo(error))
+            body("message", CoreMatchers.equalTo(message))
+            body("path", CoreMatchers.equalTo(path))
+        }
     }
 
 }
